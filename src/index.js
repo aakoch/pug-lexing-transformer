37ed231a83bc427005f3aabe26909ca2142d9f92
Adam Koch <adam@dogtired.org>
2021-12-16 23:59:44 -0600

    replaced state with indentState

--- src/index.js
+++ src/index.js
@@ -15,7 +15,6 @@ import FooDogIndentState from './fooDogIndentState.js'
 class LexingTransformer extends stream.Transform {
   first = true;
   currentIndent = 0;
-  state = []
   stack = {
     internalArr: [],
     debug: debugFunc('stack'),
@@ -39,7 +38,7 @@ class LexingTransformer extends stream.Transform {
   useAbsolutePath = true
   filesToAlsoParse = []
   override
-  indentState = new FooDogIndentState()
+  state = new FooDogIndentState()
 
   constructor(options) {
     super({ decodeStrings: true, encoding: 'utf-8' })
@@ -140,7 +139,7 @@ class LexingTransformer extends stream.Transform {
 
   handleDents(matches, ret) {
     if (matches.groups.INDENT) {
-      this.indentState.indent()
+      this.state.indent()
 
       ret.push(', "children":[');
       this.stack.push({ obj: 'children', symbol: ']' });
@@ -148,24 +147,24 @@ class LexingTransformer extends stream.Transform {
       // transformerDebug('matches.groups.INDENT=', matches.groups.INDENT)
       this.currentIndent++;
 
-
-      if (this.state.peek() == 'TEXT_START') {
-        this.state.pop();
-        this.state.push('TEXT');
-      }
-      else if (this.state.peek() == 'TEXT') {
-        this.state.push('TEXT');
-      }
-      else if (this.state.peek() == 'CODE_START') {
-        this.state.pop();
-        this.state.push('UNBUF_CODE');
-      }
-      else if (this.state.peek() == 'UNBUF_CODE') {
-        this.state.push('UNBUF_CODE');
-      }
+      
+      // if (this.state.peek() == 'TEXT_START') {
+      //   this.state.pop();
+      //   this.state.push('TEXT');
+      // }
+      // else if (this.state.peek() == 'TEXT') {
+      //   this.state.push('TEXT');
+      // }
+      // else if (this.state.peek() == 'CODE_START') {
+      //   this.state.pop();
+      //   this.state.push('UNBUF_CODE');
+      // }
+      // else if (this.state.peek() == 'UNBUF_CODE') {
+      //   this.state.push('UNBUF_CODE');
+      // }
     }
     else if (matches.groups.DEDENT) {
-      this.indentState.dedent()
+      this.state.dedent()
 
       ret.push(this.stack.pop().symbol + this.stack.pop().symbol);
 
@@ -175,13 +174,15 @@ class LexingTransformer extends stream.Transform {
 
       this.currentIndent--;
 
-      let lastState = this.state.pop();
+      // let lastState = this.state.pop();
       // if (lastState === 'MULTI_LINE_ATTRS') {
       //   this.state.push('MULTI_LINE_ATTRS_END');
       // }
+
+      // this.state.pop();
     }
     else {
-      this.indentState.nodent()
+      this.state.nodent()
 
       // need to handle the very first element
       transformerDebug('stack=', this.stack);
@@ -192,27 +193,35 @@ class LexingTransformer extends stream.Transform {
         ret.push(this.stack.pop().symbol + ', ');
       }
 
-      if ((this.state.peek() ?? '').endsWith('_START')) {
-        this.state.pop();
-      }
+      // if (this.state.peek()?.endsWith('_START')) {
+      //   this.state.pop();
+      // }
     }
   }
 
   parseLineAndCreateString(matches, ret) {
     const text = matches.groups.text;
-    transformerDebug(text);
+    // transformerDebug(text);
     if (text.trim().length > 0) {
-      transformerDebug('before state=', this.state);
-      const newObj = this.analyzeLine((this.state.length > 0 ? '<' + this.state.peek() + '>' : '') + text);
+      transformerDebug('before state=', this.state.state, ', length=', this.state.length, ', typeof=', typeof this.state.currentState);
+      let newObj
+      if (!!this.state.state.length) {
+        newObj = this.analyzeLine('<' + this.state.pop() + '>' + text);
+      }
+      else {
+        newObj = this.analyzeLine(text);
+      }
 
       transformerDebug('newObj=', newObj)
       let nestedChildren = '';
       if (newObj.hasOwnProperty('state')) {
-        this.indentState.setNewState(newObj.state)
+        transformerDebug('returned state=', newObj.state);
+        this.state.setNewState(newObj.state)
 
         if (newObj.state == 'NESTED') {
           if (newObj.children[0].hasOwnProperty('state')) {
-            this.state.push(newObj.children[0].state);
+            // this.state.push(newObj.children[0].state);
+            this.state.setNewState(newObj.children[0].state)
           }
           const childrenStr = JSON.stringify(newObj.children[0]);
           transformerDebug('childrenStr=' + childrenStr);
@@ -239,7 +248,6 @@ class LexingTransformer extends stream.Transform {
       //   newObj.children = (newObj.children || []).push(inlineParsed)
       // }
 
-      transformerDebug('after state=', this.state);
       delete newObj.state;
 
       let sourceFile = (this.override ?? (this.useAbsolutePath ? path.resolve(this.filename) : path.relative('', this.filename)))
